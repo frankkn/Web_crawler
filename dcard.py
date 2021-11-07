7df36485a1cfb702c5d2c435210ecbe6b96f8658
frankkn <frank840629@gmail.com>
2021-11-07 23:08:11 +0800

    first commit

---
+++ dcard.py
@@ -0,0 +1,15 @@
+import urllib.request as req
+url = "https://www.dcard.tw/f/nthu"
+request = req.Request(url, headers = {
+    "User-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+})
+with req.urlopen(request) as response:
+    data=response.read().decode("utf-8")
+
+
+import bs4
+root = bs4.BeautifulSoup(data, "html.parser")
+titles = root.find_all("h2", class_="tgn9uw-2 jWUdzO")
+with open("dcard.txt", mode="w", encoding="utf-8") as file:
+    for title in titles:
+        file.write(title.a.string+"\n")
